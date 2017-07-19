3d19975f4787cf83d702275611ca33f32501aed4
LT <~~~~>
2017-07-19 17:37:22 +0800

    update

---
+++ Batch/python3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python3.photo.sort',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='LT',
+    author_email='',
+    description=''
+)
