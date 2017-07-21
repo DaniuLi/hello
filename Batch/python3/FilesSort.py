09fa8d10189d6c61e93fa161c9ba854d34c64f9d
LT <~~~~>
2017-07-21 15:21:48 +0800

    update

--- Batch/python3/FilesSort.py
+++ Batch/python3/FilesSort.py
@@ -2,20 +2,17 @@
 # -*- coding: UTF-8 -*-
 
 import fnmatch
-import json
 import logging
 import logging.handlers
 import os
-import time
 import shutil
-import exifread
-from exifread import __version__, process_file
-from exifread.tags import DEFAULT_STOP_TAG, FIELD_TYPES
+import time
 from tkinter import *
+from tkinter import Radiobutton, messagebox
 from tkinter.filedialog import askdirectory
-from tkinter import messagebox
-from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
+
+import exifread
 
 
 def all_files(root, patterns='*', single_level=False, yield_folders=False):
@@ -32,26 +29,35 @@ def all_files(root, patterns='*', single_level=False, yield_folders=False):
                 if single_level:
                     break
 
+
 def sort_files(paths):
     #sortedDir = os.curdir + os.sep + "sorted" + time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()));
-    sortedDir = desPath.get() + os.sep + time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()));
+    sortedDir = desPath.get() + os.sep + time.strftime('%Y%m%d%H%M%S',
+                                                       time.localtime(time.time()))
     index = 1
     for path in all_files(paths, '*.*'):
         f = open(path[2:], 'rb')
-        tags = exifread.process_file(f,stop_tag='EXIF DateTimeOriginal')
+        tags = exifread.process_file(f, stop_tag='EXIF DateTimeOriginal')
         time_of_create = ""
         for tag in tags.keys():
-            #if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
+            # if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
             #print ("Key: %s, value %s" % (tag, tags[tag]))
             if tag == 'EXIF DateTimeOriginal':
-                time_of_create = str(tags['EXIF DateTimeOriginal'])[0:10].replace(':', '')
+                if radioVar.get() == 1:
+                    time_of_create = str(tags['EXIF DateTimeOriginal'])[
+                        0:8].replace(':', '')
+                elif radioVar.get() == 2:
+                    time_of_create = str(tags['EXIF DateTimeOriginal'])[
+                        0:10].replace(':', '')
                 break
 
         if time_of_create == "":
-            time_of_create = time.strftime("%Y%m%d", time.localtime(os.path.getmtime(path)))
-
-        logging.info("copy %s to %s" % (path, time_of_create))
-        logText.insert(END, "copy %s to %s \n" % (path, time_of_create))
+            if radioVar.get() == 1:
+                time_of_create = time.strftime(
+                    "%Y%m", time.localtime(os.path.getmtime(path)))
+            elif radioVar.get() == 2:
+                time_of_create = time.strftime(
+                    "%Y%m%d", time.localtime(os.path.getmtime(path)))
 
         dateDir = sortedDir + os.sep + time_of_create
         if os.path.exists(sortedDir) == False:
@@ -59,17 +65,22 @@ def sort_files(paths):
         if os.path.exists(dateDir) == False:
             os.mkdir(dateDir)
 
-        shutil.copy2(path,dateDir)
+        logging.info("copy %s to %s" % (path, dateDir))
+        logText.insert(END, "copy %s to %s \n" % (path, dateDir))
+        logText.update()
+        shutil.copy2(path, dateDir)
 
         index += 1
 
+    logText.insert(END, "Total files:%d \n" % (index))
+
 
 def initLog():
-    # log maxsize:10M count:5 日志文件生成脚本存放的目录
+    # log maxsize:10M count:5
     if os.path.exists("./log") == False:
         os.mkdir(os.curdir + os.sep + "log")
 
-    handler = logging.handlers.RotatingFileHandler("./log/photoSort.log",
+    handler = logging.handlers.RotatingFileHandler("./log/sort.log",
                                                    maxBytes=1073741824,
                                                    backupCount=5)
     datefmt = '%Y-%m-%d %H:%M:%S'
@@ -81,69 +92,77 @@ def initLog():
     clean_logger.setLevel(logging.INFO)
     clean_logger.addHandler(handler)
 
+
 def sort():
-    # 非定量进度条
-    #flag = False  # 标志位
-    #value = 0  # 进度条位置
 
-    #p2 = ttk.Progressbar(root, length=200, mode="indeterminate", orient=HORIZONTAL)
-    #p2.grid(row=2, column=1)
-
-    #p2.start(10)
     logging.info("=============Sort job go!=================")
-    logText.delete(1.0,END)
-    logText.insert(END, '================Sort job go!====================\n')
-    ##all_files("C:\workspace\photosort")
+    logText.delete(1.0, END)
+    logText.insert(END, '================go!====================\n')
     sort_files(srcPath.get())
     logging.info("=============Sort job done!=================\n")
-    logText.insert(END, '================Sort job done!====================\n')
-    #p2.stop()
-    #p2.grid_remove()
+    logText.insert(END, '================done!====================\n')
     logText.see(END)
-    messagebox.askokcancel('结果提示','整理完毕')
+    messagebox.askokcancel('结果提示', '整理完毕')
+
 
 def selectSrcPath():
     srcPath.set(askdirectory())
+
 
 def selectDesPath():
     desPath.set(askdirectory())
 
 
-root = Tk()
+if __name__ == '__main__':
 
-initLog()
+    root = Tk()
 
-root.wm_title("文件整理工具")
+    initLog()
 
-srcPath = StringVar()
-desPath = StringVar()
+    root.wm_title("文件整理工具")
+
+    srcPath = StringVar()
+    desPath = StringVar()
+    desPathFormat = "%Y%m"
+    desPathLength = 8
+
+    radioVar = IntVar()
+
+    labelSrcPath = Label(root, text="待整理目录:")
+    labelSrcPath.grid(row=0, column=0, sticky=W)
+    entrySrcPath = Entry(root, textvariable=srcPath, width=60)
+    entrySrcPath.grid(row=0, column=1, columnspan=2, sticky=E)
+    buttonSrcPath = Button(root, text="目录选择", width=10, command=selectSrcPath)
+    buttonSrcPath.grid(row=0, column=3, sticky=E)
+
+    labelDesPath = Label(root, text="输出目录:")
+    labelDesPath.grid(row=1, column=0, sticky=W)
+    entryDesPath = Entry(root, textvariable=desPath, width=60)
+    entryDesPath.grid(row=1, column=1, columnspan=2, sticky=E)
+    buttonDesPath = Button(root, text="目录选择",  width=10, command=selectDesPath)
+    buttonDesPath.grid(row=1, column=3, sticky=E)
+
+    RadioYM = Radiobutton(
+        root, text="按月分类", variable=radioVar, value=1)
+    RadioYM.grid(row=2, column=1, sticky=W)
+    RadioYM.select()
+
+    RadioYMD = Radiobutton(
+        root, text="按天分类", variable=radioVar, value=2)
+    RadioYMD.grid(row=2, column=2, sticky=W)
+
+    buttonSort = Button(root, text="开始", width=10, command=sort)
+    buttonSort.grid(row=2, column=3,  sticky=E)
+
+    logText = ScrolledText(root)
+    logText.grid(row=3, columnspan=4, sticky=W)
+    logText.insert(END, '\n\n')
+    logText.insert(
+        END, '         ================说明====================\n\n\n\n')
+    logText.insert(END, '1、把待整理目录下所有文件按时间分类，并拷贝到输出目录下按(月/天)分目录保存\n\n')
+    logText.insert(END, '2、如果文件为照片，且带有EXIF信息，则按EXIF中的照片拍摄时间信息进行分类\n')
+
+    root.mainloop()
 
 
-labelSrcPath = Label(root, text = "整理目录:")
-labelSrcPath.grid(row = 0, column = 0, sticky = W)
-entrySrcPath = Entry(root,textvariable=srcPath, width=50)
-entrySrcPath.grid(row = 0, column = 1, sticky = E)
-buttonSrcPath = Button(root, text ="目录选择", width = 10, command = selectSrcPath)
-buttonSrcPath.grid(row = 0, column = 2, sticky = E)
-
-labelDesPath = Label(root, text = "输出目录:")
-labelDesPath.grid(row = 1, column = 0, sticky = W)
-entryDesPath = Entry(root,textvariable=desPath, width=50)
-entryDesPath.grid(row = 1, column = 1, sticky = E)
-buttonDesPath = Button(root, text ="目录选择",  width = 10, command = selectDesPath)
-buttonDesPath.grid(row = 1, column = 2, sticky = E)
-
-buttonSort = Button(root, text ="开始", width = 10, command = sort)
-buttonSort.grid(row = 2, column = 2,  sticky = E)
-
-#s = Scrollbar(root)
-logText = ScrolledText(root)
-logText.grid(row = 3, columnspan = 3, sticky = W)
-#logText.focus_set()
-#s.config(command=logText.yview)
-#logText.config(yscrollcommand=s.set)
-
-
-root.mainloop()
-
-
+#pyinstaller -F -w -p C:\Python35-32\Scripts FilesSort.py
